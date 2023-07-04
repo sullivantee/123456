e8ece378deebfd4770d7273413c1a47ad306f239
sullivantee <106914190+sullivantee@users.noreply.github.com>
2023-07-04 16:20:22 +0800

    js

---
+++ js/showPassword.js
@@ -0,0 +1,53 @@
+function showAdminPwd() {
+    var x = document.getElementById("add_admin_pwd");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function showTeacherPwd() {
+    var x = document.getElementById("add_teacher_pwd");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function showStudentPwd() {
+    var x = document.getElementById("add_student_pwd");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function showEditAdminPwd() {
+    var x = document.getElementById("edit_admin_pwd");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function showEditTeacherPwd() {
+    var x = document.getElementById("edit_tch_pwd");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
+
+function showEditStudentPwd() {
+    var x = document.getElementById("edit_std_pwd");
+    if (x.type === "password") {
+        x.type = "text";
+    } else {
+        x.type = "password";
+    }
+}
